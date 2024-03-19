aa9bf7307e4ce91b5ce4c29ac899e3b0b53cf80a
Domagoj <paulic.domagoj@gmail.com>
2024-03-19 08:53:14 +0100

    access raw nodejs api

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve, type HttpBindings } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+type Bindings = HttpBindings & {};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.json({
+    remoteAddress: c.env.incoming.socket.remoteAddress,
+  });
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
-
-serve({
-  fetch: app.fetch,
-  port
-})
+serve(app);
