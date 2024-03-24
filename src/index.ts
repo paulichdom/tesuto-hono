9163823099be0d2a1e63efd20617b64180cc146b
Domagoj <paulic.domagoj@gmail.com>
2024-03-24 09:38:13 +0100

    serve static files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { serve, type HttpBindings } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono';
 
 type Bindings = HttpBindings & {};
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+//app.use('/static/*', serveStatic({ root: './' }));
+
+app.get(
+  '/static/*',
+  serveStatic({
+    root: './',
+    rewriteRequestPath: (path) => path.replace(/^\/static/, '/statics'),
+  })
+);
 
 app.get('/', (c) => {
   return c.json({
